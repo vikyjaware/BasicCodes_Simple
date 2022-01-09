8d1b965d1a7753e50c447b45582d93abefc47df9
Viky <97377174+vikyjaware@users.noreply.github.com>
2022-01-09 14:56:18 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ class HelloWorld {
 	public static void main(String[] args) {
 		while(true)
 		{
-			System.out.println("Hello World!!!");
+			System.out.println("Hello World !!!!");
 			try {
 				Thread.sleep(5000);
 			} catch(Exception e){
